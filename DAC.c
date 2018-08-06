3bfd55a601518c9047b02b3fcbbeb07dc78307ac
Bellabaci Nazim <30374106+NazimBL@users.noreply.github.com>
2018-08-06 22:43:01 +0200

    Create DAC.c
    MCP4822 DAC interfacing

---
+++ DAC.c
@@ -0,0 +1,37 @@
+void Setup();
+void writeDAC(unsigned dac);
+
+char lowB,highB;
+
+void main() {
+
+Setup();
+
+while(1){
+
+         writeDAC(2000);      
+         Delay_ms(500);
+
+}
+
+}
+void Setup(){
+
+DDB0_bit = 1;
+
+SPI1_Init_Advanced(_SPI_MASTER,_SPI_FCY_DIV32,_SPI_CLK_LO_LEADING);
+
+}
+
+void writeDAC(unsigned dac){
+
+      lowB=dac & 0xFF;
+      highB=dac >> 8;
+      //add command word
+      highB+=16;
+      PORTB0_bit=0;
+      SPI1_Write(highB);
+      SPI1_Write(lowB);
+      
+      PORTB0_bit=1;
+}
