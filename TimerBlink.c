7624ecbb03f861bb10953188621ba9305743fc19
Bellabaci Nazim <30374106+NazimBL@users.noreply.github.com>
2018-08-11 04:50:09 +0200

    Create TimerBlink.c

---
+++ TimerBlink.c
@@ -0,0 +1,25 @@
+
+void Timer_Setup();
+
+void Timer0Overflow_ISR() org IVT_ADDR_TIMER0_OVF {
+
+  PORTB0_bit=~PORTB0_bit;
+  
+}
+
+
+void main() {
+
+DDB0_bit = 1;
+PORTB0_bit=0;
+
+}
+
+
+void Timer_Setup(){
+
+SREG_I_bit = 1;               // Interrupt enable
+TOIE0_bit  = 1;               // Timer0 overflow interrupt enable
+TCCR0  = 5;                   // Start timer with 1024 prescaler
+
+}
