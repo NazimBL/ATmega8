87584e5788eeecdc17fb7c1c93274fb903ed2bda
Bellabaci Nazim <30374106+NazimBL@users.noreply.github.com>
2019-11-03 12:06:26 +0100

    Update and rename TimerBlink.c to Timer.c

--- Timer.c
+++ Timer.c
@@ -1,10 +1,14 @@
+/*
+Nazim BL
+mikroC PRO for AVR
+Blinking LED through Timer
+*/
 
 void Timer_Setup();
 
+//Timer ISR
 void Timer0Overflow_ISR() org IVT_ADDR_TIMER0_OVF {
-
-  PORTB0_bit=~PORTB0_bit;
-  
+  PORTB0_bit=~PORTB0_bit;  
 }
 
 
@@ -12,7 +16,6 @@ void main() {
 
 DDB0_bit = 1;
 PORTB0_bit=0;
-
 }
 
 
@@ -21,5 +24,4 @@ void Timer_Setup(){
 SREG_I_bit = 1;               // Interrupt enable
 TOIE0_bit  = 1;               // Timer0 overflow interrupt enable
 TCCR0  = 5;                   // Start timer with 1024 prescaler
-
 }
