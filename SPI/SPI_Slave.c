0c00c5f77c3742d8143bb6509da3a8a1c79105d6
Bellabaci Nazim <30374106+NazimBL@users.noreply.github.com>
2019-11-03 12:04:01 +0100

    Create SPI_Slave.c

---
+++ SPI/SPI_Slave.c
@@ -0,0 +1,34 @@
+/*
+ Bellabaci Nazim 30/12/2017 
+ DeadLine Babeee
+ CodeBlocks
+*/
+
+#include <avr/io.h>
+#include "myADC.h"
+#include "MySpi.h"
+
+int main(void)
+{
+
+    ADC_Init();
+    spi_SlaveInit();
+
+    int value;
+    char lowB,highB;
+
+     DDRD=0xFF;
+
+    while(1){
+
+
+        value=ADC_Read(0);
+        lowB=value;
+        highB=(value>>8);
+        SPI_Write(lowB);
+        SPI_Write(highB);
+
+    }
+
+    return 0;
+}
