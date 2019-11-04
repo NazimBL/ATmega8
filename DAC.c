8bf6dbc5cf9326b58efe4528a12ea6e92be77f05
Bellabaci Nazim <30374106+NazimBL@users.noreply.github.com>
2019-11-04 08:53:23 +0100

    Update DAC.c

--- DAC.c
+++ DAC.c
@@ -6,21 +6,14 @@ char lowB,highB;
 void main() {
 
 Setup();
-
-while(1){
-
-         writeDAC(2000,1);      
-         Delay_ms(500);
-
+writeDAC(2000,1);
+         
 }
 
-}
 void Setup(){
 
 DDB0_bit = 1;
-
 SPI1_Init_Advanced(_SPI_MASTER,_SPI_FCY_DIV32,_SPI_CLK_LO_LEADING);
-
 }
 
 void writeDAC(unsigned dac){
@@ -29,10 +22,10 @@ void writeDAC(unsigned dac){
       highB=dac >> 8;
       //add command word
       highB+=16;
-      highB+=(channel*128);  
+      highB+=(channel*128); 
+         
       PORTB0_bit=0;
       SPI1_Write(highB);
       SPI1_Write(lowB);
-      
       PORTB0_bit=1;
 }
